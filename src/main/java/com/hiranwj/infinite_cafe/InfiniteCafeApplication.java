f22d5ad403802a9b782b21b18c648e38332a7e31
hiran_xdoto <hiran@xdoto.io>
2025-02-17 14:21:15 +0530

    Initial commit

---
+++ src/main/java/com/hiranwj/infinite_cafe/InfiniteCafeApplication.java
@@ -0,0 +1,13 @@
+package com.hiranwj.infinite_cafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfiniteCafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfiniteCafeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hiranwj/infinite_cafe/InfiniteCafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiranwj.infinite_cafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfiniteCafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
